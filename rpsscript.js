63728104da010bff2178f82ebca0216139e76612
Apachemann <shawnkorns@mac.com>
2022-09-22 18:03:58 -0500

    completed functionality for best of 5 game

--- rpsscript.js
+++ rpsscript.js
@@ -1,5 +1,5 @@
 function getComputerChoice() {
-    var choices = ["Rock", "Paper", "Scissors"];
+    var choices = ["rock", "paper", "scissors"];
     var random = Math.floor((Math.random() * choices.length));
     var randomChoice = choices[random];
     return randomChoice;
@@ -12,31 +12,74 @@ function playRound(playerSelection, computerSelection) {
     computerSelection = computerSelection.toLowerCase();
 
     if (playerSelection == "rock" && computerSelection == "rock") {
-        console.log("Draw! Rock ties with Rock");
+        return "Draw! Rock ties with Rock";
     } else if (playerSelection == "rock" && computerSelection == "scissors") {
-        console.log("You win! Rock beats scissors");
+        return "You win! Rock beats scissors";
     } else if (playerSelection == "rock" && computerSelection == "paper") {
-        console.log("You lose! Paper beats rock");
+        return "You lose! Paper beats rock";
     }
 
     if (playerSelection == "paper" && computerSelection == "paper") {
-        console.log("Draw! Paper ties with Paper");
+        return "Draw! Paper ties with Paper";
     } else if (playerSelection == "paper" && computerSelection == "rock") {
-        console.log("You win! Paper beats rock");
+        return "You win! Paper beats rock";
     } else if (playerSelection == "paper" && computerSelection == "scissors") {
-        console.log("You lose! Scissors beats paper");
+        return "You lose! Scissors beats paper";
     }
 
     if (playerSelection == "scissors" && computerSelection == "scissors") {
-        console.log("Draw! Scissors ties with scissors");
+        return "Draw! Scissors ties with scissors";
     } else if (playerSelection == "scissors" && computerSelection == "paper") {
-        console.log("You win! Scissors beats paper");
+        return "You win! Scissors beats paper";
     } else if (playerSelection == "scissors" && computerSelection == "rock") {
-        console.log("You lose! Rock beats scissors");
+        return "You lose! Rock beats scissors";
     }  
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
+function game() {
+    // Play 5 rounds of RPS and determine a winner at the end
+    for (let i = 0; i < 5; i++) {
+        // Ask for the player's choice
+        let playerChoice = prompt("Choose rock, paper, or scissors.");
+        
+        // Format the response to lower case
+        playerChoice = playerChoice.toLowerCase();
 
-console.log(playRound(playerSelection, computerSelection));
+        // Get the computer's choice
+        let computerChoice = getComputerChoice();
+
+        // Play a round
+        let outcome = playRound(playerChoice, computerChoice);
+
+        // Determine the round winner
+        if (outcome.startsWith("You win!")) {
+            // Increment your score
+            playerScore += 1;
+
+            // Output the results of the round
+            console.log(outcome);
+        } else if (outcome.startsWith("You lose!")) {
+            // Increment the computer's score
+            computerScore += 1;
+
+            // Output the results of the round
+            console.log(outcome);
+        } else if (outcome.startsWith("Draw!")) {
+
+            // Output the results of the round
+            console.log(outcome);
+        }
+    }
+
+    // Display the winner of the game
+    if (playerScore > computerScore) {
+        console.log("You won the game!");
+    } else if (computerScore > playerScore) {
+        console.log("You lost the game!");
+    } else {
+        console.log("The game is a draw!");
+    }
+}
+
+var playerScore = 0;
+var computerScore = 0;
